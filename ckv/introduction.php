f824e744711217b17ba945b6bfd0a739576bccc8
Robin Boers <robindev2019@outlook.com>
2022-09-25 19:33:36 +0200

    Format & add introductielessen

---
+++ ckv/introduction.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Introductielessen — CKV | Robin Boers</title>
+
+        <link rel="stylesheet" href="/ckv/assets/css/ckv.css" type="text/css" />
+        <meta name="robots" content="noindex, nofollow" />
+    </head>
+
+    <body>
+        <header>
+            <p><a href="/ckv">ЯӨBIП</a></p>
+        </header>
+        <main class="content center">
+            <article>
+                <section class="box">
+                    <h1>Introductielessen</h1>
+
+                    <p>
+                        Deze page bevat onderzoeksopdrachten en bijbehorende
+                        resultaten van de
+                        <a
+                            href="https://ckvzan.wixsite.com/dossier/kopie-van-blok-1-1"
+                            >introductielessen</a
+                        >. Dit is momenteel nog een WIP.
+                    </p>
+
+                    <h2 id="fotografie">Fotografie</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>vorm</th>
+                                <th>inhoud</th>
+                                <th>materiaal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    twee appels met een steeltje ertussen,
+                                    standpunt vanaf boven
+                                </td>
+                                <td>liefde (suggereert seksualiteit)</td>
+                                <td>appels, camera</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    twee om elkaar gewonden wortels die in het
+                                    gras liggen, kroon buiten beeld (afsnijding)
+                                </td>
+                                <td>liefde (suggereert ook seksualiteit)</td>
+                                <td>wortels, camera</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <h3>Wat als je de vorm of het materiaal zou veranderen?</h3>
+                    <p>
+                        Als je de vorm zou veranderen zou de foto een andere
+                        inhoud krijgen.
+                    </p>
+                    <p>
+                        Als je de appels anders neerlegt is er geen suggestie
+                        van seksualiteit. Als je de wortels niet in het gras zou
+                        hebben liggen zou het ook al veel minder emotie
+                        oproepen. Ook als je de belichting aanpast zou de foto
+                        al een andere &quot;vibe&quot; krijgen waardoor de
+                        boodschap anders wordt overgebracht.
+                    </p>
+                    <p>
+                        Als je met een andere lens of camera fotografeert krijg
+                        je ook andere (kleur)contrasten die de boodschap
+                        beïnvloeden.
+                    </p>
+                    <h3>
+                        Hoe verschilt het materiaal en de vorm tussen de
+                        foto&#39;s?
+                    </h3>
+                    <p>
+                        Beide foto&#39;s maken gebruik van eten om seksualiteit
+                        te suggereren. Wel is de eerste foto met de appels
+                        zwart-wit en de foto met de wortels in kleur gemaakt.
+                        Ook is de foto van de wortels in landscape gemaakt en is
+                        de foto van de appels in portrait. Daardoor voelt de
+                        foto van de appels &quot;krapper&quot; aan dan die met
+                        de wortels, die is namelijk wat opener.
+                    </p>
+                    <p>
+                        Het effect is wel dat ik de foto van de appels meer
+                        ervaar als een symbolisch ding dan de foto van de
+                        wortels.
+                    </p>
+
+                    <h2 id="performance">Performance</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>vorm</th>
+                                <th>inhoud</th>
+                                <th>materiaal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>elkaar diep in de ogen aankijken</td>
+                                <td>
+                                    liefde (ze hadden een relatie gehad en zagen
+                                    elkaar voor het eerst na zoveel jaar)
+                                </td>
+                                <td>lichaam, grote hal met stoelen</td>
+                            </tr>
+                            <tr>
+                                <td>twee naakte mensen in de deurpost</td>
+                                <td>
+                                    liefde (het uitzoeken van hoe het
+                                    &quot;publiek&quot; zou reageren)
+                                </td>
+                                <td>lichaam, deurpost</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <h3>Wat als je de vorm of het materiaal zou veranderen?</h3>
+                    <p>
+                        Als je de vorm zou veranderen zou de performance een
+                        andere uitwerking op het publiek hebben (een andere
+                        emotie opbrengen).
+                    </p>
+                    <p>
+                        Als je ipv. elkaar in de ogen aan te kijken hadden ze
+                        ook een gesprek kunnen voeren aan de keukentafel, maar
+                        dat roept toch andere emoties op. De performance bij de
+                        deurpost had ook een totaal andere uitwerking op het
+                        publiek gehad als ze niet naakt geweest waren.
+                    </p>
+                    <p>
+                        Het materiaal ligt natuurlijk al vast, want dat is je
+                        eigen lichaam, de vorm bepaalt echter wat de precieze
+                        boodschap is (het aspect van de liefde) die de
+                        performance laat zien (al kan de omgeving waarin de
+                        performance wordt uitgevoerd — materiaal — ook wel
+                        uitmaken).
+                    </p>
+                    <h3>
+                        Hoe verschilt het materiaal en de vorm tussen de
+                        performances?
+                    </h3>
+                    <p>
+                        Het materiaal is, zoals ik hierboven als zei, bij beide
+                        performances je eigen lichaam. De omgeving verschilt
+                        wel, de performance van de naakte mensen in de deurpost
+                        had niet gewerkt in de grote hal met stoelen (zonder de
+                        deurpost), en ook niet als niemand door die deur zou
+                        willen lopen.
+                    </p>
+                    <p>
+                        Verder is de vorm natuurlijk verschillend omdat de
+                        performances een ander aspect van de liefde beschrijven.
+                    </p>
+
+                    <h2 id="verschillen-tussen-de-disciplines">
+                        Verschillen tussen de disciplines
+                    </h2>
+                    <p>
+                        Bij beide disciplines stond de inhoud redelijk vast: een
+                        aspect van de liefde. Het materiaal daarentegen was bij
+                        performance altijd je eigen lichaam, en bij fotografie
+                        was het vaker apparatuur zoals lichten, lenzen en
+                        camera&#39;s. Bij fotografie kan je ook werken met wat
+                        er wel/niet in frame is (vorm), bij performance heb je
+                        dat aspect niet. Je kan bijvoorbeeld daarmee nadruk
+                        leggen op dingen, zoals de handen in de volgende foto:
+                    </p>
+                    <figure>
+                        <img
+                            src="/ckv/assets/images/belichting-focus.jpg"
+                            alt="Belichting kan nadruk leggen op het onderwerp van de foto" />
+                        <figcaption>
+                            Bron:
+                            <a href="https://citrinitas.com">Citrinitas</a>
+                        </figcaption>
+                    </figure>
+                    <p>
+                        Je kan ook werken met de manier van afdrukken en
+                        verschillen in belichting en lens. Met belichting kan je
+                        de focus op het onderwerp van de foto leggen. Je kan ook
+                        met effecten/filters bepaalde kleurcontrasten versterken
+                        zodat de emotie die een foto oproept veranderd:
+                    </p>
+                    <figure>
+                        <img
+                            src="/ckv/assets/images/fotografie-filters.jpg"
+                            alt="Filters kunnen de emotie die een foto opwekt versterken" />
+                        <figcaption>
+                            Bron:
+                            <a href="https://mihoci.com">MihociStudios</a>
+                        </figcaption>
+                    </figure>
+                    <p>
+                        Bij performance kan je echter dingen als beweging beter
+                        benutten. Door bepaalde bewegingen/lichaamshoudingen te
+                        gebruiken kan je de inhoud overbrengen. Denk
+                        bijvoorbeeld aan het imiteren van een handeling (iets
+                        naspelen, zoals bijvoorbeeld autorijden of koken).
+                    </p>
+                </section>
+            </article>
+            <aside class="sidebar">
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="/ckv">Start</a></li>
+                        <li><a href="/ckv/about">Over mij</a></li>
+                        <li>
+                            <a href="/ckv/introduction">Introductielessen</a>
+                        </li>
+                        <li><a href="/ckv/blok/1">Blok 1</a></li>
+                        <li><a href="/ckv/blok/2">Blok 2</a></li>
+                        <li><a href="/ckv/blok/3">Blok 3</a></li>
+                        <li><a href="/ckv/blok/4">Blok 4</a></li>
+                        <li>
+                            <a href="/ckv/initiatives">Mijn initiatieven</a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        </main>
+        <footer>
+            <p>
+                Made with ♡ by Robin Boers.
+                <a href="https://copyheart.org">Copying is an act of love</a>.
+                Please copy.
+            </p>
+        </footer>
+    </body>
+</html>
---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Starts local server for development
+
+echo "Serving at http://localhost:4000/ckv/"
+php -S 0.0.0.0:4000 router.php
