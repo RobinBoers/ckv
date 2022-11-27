ed9a969c653109b0302142e89b4d12a7cebf9aad
Robin Boers <robindev2019@outlook.com>
2022-11-27 12:04:22 +0100

    Add blok 1

--- ckv/blok/1.php
+++ ckv/blok/1.php
@@ -19,25 +19,133 @@
                 <section class="box">
                     <h1>Blok 1</h1>
                     <p>
-                        <i
-                            >Sed sed dui lacus. Duis maximus volutpat orci, sed
-                            volutpat metus tempus sed. Phasellus id aliquam
-                            risus, molestie euismod tortor. Etiam egestas
-                            volutpat lectus ac maximus. Curabitur rhoncus vitae
-                            sem eu dictum. Integer a quam sit amet neque congue
-                            rhoncus et vel tortor. Integer risus neque, pretium
-                            eget lorem non, congue tempor lorem. Phasellus
-                            fermentum diam vel urna placerat, in accumsan augue
-                            iaculis. Praesent quis quam id justo faucibus
-                            elementum. Pellentesque habitant morbi tristique
-                            senectus et netus et malesuada fames ac turpis
-                            egestas. Cras massa arcu, porttitor sit amet turpis
-                            vel, dignissim tristique felis. Proin tempus nisl ac
-                            augue viverra, vitae porta felis posuere. Nullam
-                            luctus, justo id tincidunt ornare, leo enim interdum
-                            magna, non commodo ante dui eu quam. Quisque sit
-                            amet lacus neque. Maecenas eu dictum nibh.</i
+                        Ik heb dit blok voor filmmuziek gekozen omdat ik
+                        nieuwsgierig was hoe muziek in films ons psychologisch
+                        gezien beinvloedt. Verder hoopte ik te leren hoe
+                        bepaalde motieven/melodiën bepaalde emoties oproepen, al
+                        weet ik daar nog steeds erg weinig van.
+                    </p>
+
+                    <p>
+                        Ik kende voor deze lessen nog bijna geen filmmuiek,
+                        naast de themesongs dan. Ik ken nu iets meer filmmuziek
+                        (vooral dankzij
+                        <a href="https://www.youtube.com/watch?v=uzSHplbvdmU"
+                            >de DWDD video van JunkieXL</a
                         >
+                        die we in tijdens de les hebben gekeken).
+                    </p>
+                    <p>
+                        Ik had eigen nog nooit gemerkt dat veel filmmuziek uit
+                        maar één akkoord bestaat, maar ik vind het wel erg
+                        interessant hoe je met zo weinig akkoorden zulke goede
+                        muziek kan maken.
+                    </p>
+                    <p>
+                        Ik heb met Kevin, Sem en Jordy samengewerkt. We hebben
+                        het langst gedaan over de analyse van de filmtrailer en
+                        (in mijn beleving) de minste tijd besteed aan het
+                        componeren van muziek voor
+                        <a href="https://www.youtube.com/watch?v=I86bXhxkXIA"
+                            >La Linea</a
+                        >.
+                    </p>
+                    <p>
+                        We hebben op voor dit blok
+                        <em
+                            ><a
+                                href="https://en.wikipedia.org/wiki/Three_Thousand_Years_of_Longing"
+                                >Three Thousand Years of Longing</a
+                            ></em
+                        >
+                        gekeken. Ik vond dat de film niet echt een logische
+                        verhaallijn had. Hij begon met hoe de hoofdpersoon een
+                        narratoloog is en ze dus de gevaren van wensen doen erg
+                        goed kent uit de verhalen die worden verteld. Ze heeft
+                        geen verlangens en wil dus eigenlijk geen wens doen. Dan
+                        vertelt de Djinn zijn levensverhaal dat dit standpunt
+                        bevestigd en goed onderbouwt. Dan komt er ineens een 180
+                        en heeft ze ineens een erg cliché verlangen, waarna de
+                        film een beetje onsamenhangend wordt.
+                    </p>
+                    <p>
+                        Mijn mening over de verhaallijn is natuurlijk niet echt
+                        relevant in het kader van filmmuziek. Ik heb geprobeerd
+                        op de filmmuziek te letten, maar dat is erg moeilijk. De
+                        geluidseffecten vielen met wel heel erg op. Misschien is
+                        dit omdat de muziek veel minder bewust binnenkomt, veel
+                        subtieler.
+                    </p>
+                    <p>
+                        Kortom denk ik dat deze periode erg interessant was, al
+                        was het wel anders dan ik had verwacht. Ik eigenlijk
+                        bijna niks nieuws geleerd, maar het was wel leuk. Ik had
+                        alleen gehoopt meer over de opbouw en effecten van
+                        filmmuziek te leren, meer concrete informatie. Het zelf
+                        maken van muziek is een leuk extraatje, maar was niet de
+                        primaire reden dat ik dit onderwerp had gekozen.
+                    </p>
+
+                    <h2 id="zelf-gecomponeerde-muziek">
+                        Zelf gecomponeerde muziek
+                        <span class="subtitle">(praktische opdracht 4)</span>
+                    </h2>
+
+                    <p>We hebben voor de volgende strip muziek gemaakt:</p>
+
+                    <figure>
+                        <img
+                            src="/ckv/assets/images/strip.jpg"
+                            alt="Handgetekende strip met stick-figures." />
+                    </figure>
+
+                    <p>
+                        We moesten er eerst een verhaal bij verzinnen. We hebben
+                        dit bedacht:
+                    </p>
+                    <ol>
+                        <li>Een oude man zit in zijn stoel bij het raam.</li>
+                        <li>
+                            Hij probeert een kopje koffie te pakken maar valt
+                            van zijn stoel.
+                        </li>
+                        <li>
+                            Zijn kleinzoon help hem overeind. De oude man
+                            bedankt hem.
+                        </li>
+                    </ol>
+                    <p>
+                        De muziek begint rustig, waarna het tempo versnelt om de
+                        spanning op te bouwen. Op het laast wordt het tempo weer
+                        langzamer en wordt de muziek weer rustig als alles goed
+                        komt.
+                    </p>
+
+                    <audio src="/ckv/assets/music/strip.m4a" controls></audio>
+
+                    <p>
+                        <em
+                            ><small
+                                >Ons originele stuk had een pitchbend E op een
+                                tweede keyboard erdoorheen om de spanning op te
+                                bouwen, maar ik heb thuis maar een keyboard en
+                                die heeft geen pitchbend, dus zit dat niet in de
+                                opname.</small
+                            ></em
+                        >
+                    </p>
+
+                    <h2 id="vorm-inhoud-en-materiaal">
+                        Vorm, inhoud en materiaal
+                    </h2>
+                    <p>
+                        De vorm van filmmuziek staat vast en is natuurlijk
+                        audio. De inhoud kan heel erg varieren omdat je met
+                        filmmuziek allemaal soorten emoties kan oproepen. Het
+                        materiaal verschilt ook heel erg, maar je hebt meestal
+                        in ieder geval 1 soort instrument nodig. In veel
+                        gevallen werk je met ook iets van
+                        post-production/effecten.
                     </p>
                 </section>
             </article>
